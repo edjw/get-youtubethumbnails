5873618d15982168133b140fd9e13b8bf9296c12
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2018-08-29 08:54:01 +0100

    Using flask-talisman for content security policies and auto ssl

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
-from flask_sslify import SSLify
+from flask_talisman import Talisman
 
 app = Flask(__name__)
-sslify = SSLify(app)
+csp = {
+    "default-src": "'self'",
+    "style-src": ["'self'", "stackpath.bootstrapcdn.com"],
+    "img-src": ["'self'", "img.youtube.com"],
+}
+
+Talisman(app, referrer_policy="same-origin", content_security_policy=csp)
+
 from app import routes
 from config import Config
 
