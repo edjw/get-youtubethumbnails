f6c2d935dfa17f77a8f1c594e491aad505439d16
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2018-08-29 09:12:12 +0100

    added csrf mitigation through flask-seasurf

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_talisman import Talisman
+from flask_seasurf import SeaSurf
 
 app = Flask(__name__)
 csp = {
@@ -9,6 +10,7 @@ csp = {
 }
 
 Talisman(app, referrer_policy="same-origin", content_security_policy=csp)
+csrf = SeaSurf(app)
 
 from app import routes
 from config import Config
